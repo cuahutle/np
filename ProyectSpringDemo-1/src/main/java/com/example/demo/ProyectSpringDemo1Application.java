bd5dace599bdb0157e4f56cce0a6fcad2306a1b1
Abraham-Cuahutle <abraham_8417@hotmail.com>
2022-10-13 23:20:42 -0500

    qwer

---
+++ ProyectSpringDemo-1/src/main/java/com/example/demo/ProyectSpringDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectSpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectSpringDemo1Application.class, args);
+	}
+
+}
